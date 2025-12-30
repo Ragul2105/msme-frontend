c8f11812dc2df9bc0fe38b5bdd9a729d9e1c21a0
ragul2105 <raguld2105@gmail.com>
2025-12-30 14:12:47 +0530

    changes

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-//const supabaseUrl = "https://esvlauuiwjvrfoehgbin.supabase.co";
-const supabaseUrl = "https://funcsftdgpcvpurfqgil.supabase.co";
-//const supabaseAnonKey =
-//  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdmxhdXVpd2p2cmZvZWhnYmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NTA1MzQsImV4cCI6MjA3ODEyNjUzNH0.n91EsijIp7P9ZyCaYu-X29qh1vTCreq8iFyLFiiehJM";
+const supabaseUrl = "https://esvlauuiwjvrfoehgbin.supabase.co";
+//const supabaseUrl = "https://funcsftdgpcvpurfqgil.supabase.co";
 const supabaseAnonKey =
-  "sb_publishable_2ybZim5ProhulnYCudaO-w_H6WQXUCb";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdmxhdXVpd2p2cmZvZWhnYmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NTA1MzQsImV4cCI6MjA3ODEyNjUzNH0.n91EsijIp7P9ZyCaYu-X29qh1vTCreq8iFyLFiiehJM";
+//const supabaseAnonKey =
+  //"sb_publishable_2ybZim5ProhulnYCudaO-w_H6WQXUCb";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
